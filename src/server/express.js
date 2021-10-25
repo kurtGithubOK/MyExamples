0a5ca8942e92d22e52b7cf6e05702e02acb77be1
Kurt Heimerman <kurtheimerman@yahoo.com>
2021-10-24 21:11:05 -0700

    post now updates db/people.json!!!

--- src/server/express.js
+++ src/server/express.js
@@ -1,9 +1,10 @@
 // Imports
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
-const PORT = 3001;
+const PORT = 3005;
 
 const peopleIknow = [
   { id: 1, name: 'kurt' },
@@ -34,45 +35,59 @@ app.get('/', (req, res) => res.send('This text sent via res.send(...)'));
 
 // How to return contents of a file.
 app.get('/getFile', (req, res) =>
+  // console.log('Request method:', req.method);
+  // console.log('Request rawHeaders:', req.rawHeaders);
   res.sendFile(path.join(__dirname, 'public/helloWorld.html'))
 );
 
 // res.json() returns stringified object.
 app.get('/getJson', (req, res) => {
-  res.json( { term: 'api', description: 'description here.' } );
+  res.json({ term: 'api', description: 'description here.' });
 });
 
 // Using query string parameters.
 app.get('/people/:id', (req, res) => {
   const requestedId = parseInt(req.params.id);
-  const foundPerson = peopleIknow.find( (person) => {
+  const foundPerson = peopleIknow.find((person) => {
     return person.id === requestedId;
   });
 
-  if(foundPerson) res.json(foundPerson);
+  if (foundPerson) res.json(foundPerson);
   else return res.json('No match found');
 });
 
 // Post requests.
 app.post('/post', (req, res) => {
-  console.log('Request method:', req.method);
-  // console.log('Request rawHeaders:', req.rawHeaders);
-
   // To get the body out of the request:
-  const requestBody = req.body;
-  console.log('requestBody:', requestBody);
-  // respond to user to complete request-response cycle.
-  res.status(200).json(requestBody);
+  const { id, name } = req.body;
+  const personToAdd = { id: id, name: name };
+
+  // Write the string to a file.  CODE ASSUMES FILE IS THERE.
+  const dbFileName = './db/people.json';
+  fs.readFile(dbFileName, 'utf8', (error, data) => {
+    if(error) console.log('Error occurred when reading file:', error);
+    
+    let people = [];
+    if (data) people = JSON.parse(data);
+    
+    people.push(personToAdd);
+
+    fs.writeFile(dbFileName, JSON.stringify(people), (error) => {
+      if (error) {
+        console.log('Error occurred when adding person', error);
+        res.status(500).json('Error occurred when adding person');
+      } else {
+        console.log('Person added:', personToAdd)
+        res.status(200).send('Person added' + JSON.stringify(personToAdd));
+      }
+    });
+
+  });
+
 });
 
-
-
-
-
-
-
 // Fallback route for when a user attempts to visit routes that don't exist
-app.get('*', (req, res) => res.send(`The URL you requested was not found`) );
+app.get('*', (req, res) => res.send(`The URL you requested was not found`));
 
 // Start server on a given port.
 app.listen(PORT, () =>
